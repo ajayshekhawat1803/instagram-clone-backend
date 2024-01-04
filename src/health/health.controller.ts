95be1b909fdeb2dd2a7b4c0fc4025140f3db7440
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2024-01-04 21:33:40 +0530

    Health check

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -6,6 +6,7 @@ export class HealthController {
 
     @Get()
     checkhealth() {
+        console.log("Health checked");
         return "this server is running well and giving proper response"
     }
 }
